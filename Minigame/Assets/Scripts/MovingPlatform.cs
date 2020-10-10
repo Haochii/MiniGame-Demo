34c263787ddc98a5346f972f38766ed9347c8175
Haochii <hawkliu.game@gmail.com>
2020-10-10 22:40:16 +0800

    Upload

---
+++ Minigame/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    public float speed;
+    public float startWaitTime; //等待时间
+    private float waitTime;
+    private bool MoveDir = false;
+    private bool IsMoving = false;
+
+    public Transform originPos;
+    public Transform targetPos;
+    // Start is called before the first frame update
+    void Start()
+    {   Debug.Log(IsMoving);
+        waitTime = startWaitTime;
+        transform.position = originPos.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartMoving();
+    }
+
+    void StartMoving(){
+        Transform movPos;
+        if(IsMoving){
+            Debug.Log("11");
+            if(MoveDir == true){
+                movPos = targetPos;
+            }else{
+                movPos = originPos;
+            }
+            Debug.Log("111");
+            transform.position = Vector2.MoveTowards(transform.position,movPos.position,speed * Time.deltaTime);
+            if(Vector2.Distance(transform.position,movPos.position)<0.1f){
+                if(waitTime <= 0){
+                    Debug.Log("1111");
+                    IsMoving = false;
+                }else{
+                    Debug.Log("11111");
+                    waitTime -= Time.deltaTime;
+                }            
+            }
+
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Ontrigger");
+        Debug.Log(IsMoving);
+        Debug.Log("1");
+        Debug.Log(other.gameObject.CompareTag("Player"));
+        Debug.Log("2");
+        if(!IsMoving && other.gameObject.CompareTag("Player")){
+            Debug.Log("Ontrigger2");
+            waitTime = startWaitTime;
+            MoveDir = !MoveDir;
+            IsMoving = true;
+        }
+    }
+
+}
---
+++ Minigame/Assets/Scripts/SpikeDown.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeDown : MonoBehaviour
+{
+    public float waitTime;
+    private PlayerHealth playerHealth;
+    private Rigidbody2D myRigidbody2D;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        myRigidbody2D.gravityScale = 0;
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(waitTime>=0){
+            waitTime -= Time.deltaTime;
+        }
+        if(waitTime<0){
+            myRigidbody2D.gravityScale = 1;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.CompareTag("Player") && other.GetType().ToString() == "UnityEngine.CapsuleCollider2D"){
+            if(playerHealth!=null){
+                playerHealth.DamagePlayer(20);        
+            }
+        }
+        
+    }
+}
